f30e43e7dbf7fa140096d8b5876f64fc7acf7d5e
Dhaval Saxena <dp90440@gmail.com>
2022-06-24 16:09:58 +0530

    #53 Update Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -46,6 +46,41 @@ router.post(
   }
 );
 
+
+// Route:3:Update a note using PUT:"/api/notes/updatenote/:id" Login Required
+router.put("/updatenote/:id",
+  fetchuser,
+  async (req, res) => {
+    const {title,description,tag}=req.body;
+    // Create a newNote Object
+    const newNote={}
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+
+    //find the note to be updated and update it
+    let note=await Note.findById(req.params.id);
+    if(!note){res.status(404).send("Not Found")} 
+    
+    if(note.user.toString()!==req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+    note=await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+    res.json({note});
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
 
 
