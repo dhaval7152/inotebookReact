7fd02f16f9fec2132e6a9decd9d176ac1e220f88
Dhaval Saxena <dp90440@gmail.com>
2022-06-28 15:13:22 +0530

    #63 AddNote function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
@@ -11,7 +12,9 @@ function App() {
       <Router>
         <div>
           <Navbar />
-        </div>
+            
+          <Alert message="Welcome To iNotebook"/>
+          </div>
       <div className="container">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
